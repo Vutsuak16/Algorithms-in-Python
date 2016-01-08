0a3594453c7fb603e58133473dcad789374bf21a
kaustuv deolal <vutsuak96@gmail.com>
2016-01-08 20:53:35 +0530

    script to multiply 2 numbers recursively

---
+++ Multiply_Recursion.py
@@ -0,0 +1,18 @@
+__name__ = "vutsuak"
+
+
+def multiply(x, y):
+    if y == 0:
+        return 0
+    else:
+        return x + multiply(x, y - 1)
+
+
+def main():
+    x = input("enter first number")
+    y = input("enter second number")
+    print multiply(x, y)
+
+
+if __name__ == "vutsuak":
+    main()
