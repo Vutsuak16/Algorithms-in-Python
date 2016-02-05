c4ade75199b48bded53efe377262b179dd2dead0
kaustuv deolal <vutsuak96@gmail.com>
2016-02-05 21:53:14 +0530

    fermat's number created

--- fermat's number.py
+++ fermat's number.py
@@ -1,5 +1,7 @@
 __name__ = "vutsuak"
 
+import math
+
 ''' This is a script to check if a number is a fermat number or not and print all fermat's number in a range
  They are expressed as Fn= 2^(2^n) +1
  They have the property that except F0 and F1 all end in 7
@@ -10,16 +12,18 @@ def check_fermats(n):
     F0 = 3
     F1 = 5
 
-    if int(n) == 3 or int(n) == 5:
+    if int(n) == F0 or int(n) == F1:
         print n + " is a Fermat's Number"
     elif n[-1] == "7":
-        print n + " is a Fermat's Number"
+        N = math.log(math.log(int(n) - 1) / math.log(2)) / math.log(2)
+        if N.is_integer():
+            print n + " is a Fermat's Number"
     else:
         pass
 
 
 def main():
-    for i in range(10000):
+    for i in range(10000000):
         check_fermats(str(i))
 
 
