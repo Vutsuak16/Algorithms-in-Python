f6b109538fdce1913cdf926ccdb05b3d98c91c22
kaustuv deolal <vutsuak96@gmail.com>
2016-03-01 15:08:54 +0530

    divisibily by 7

---
+++ divisibility_by_7.py
@@ -0,0 +1,15 @@
+__name__ = "vutsuak"
+
+#logic : if digits(without last digit)-(2*last_digit )is divisible by  then number is divisible by 7
+import random
+
+n = random.randrange(1, 1000)
+last_digit = n % 10
+n = str(n)
+print n
+x = int(n[:-1])
+if (x - (2 * last_digit)) % 7 == 0:
+    print n + " is divisible by 7"
+else:
+    print n + " is not divisible by 7 "
+
